0f33ca37a474950990034b38d436ef83ec2fa9ee
Reupiey <p.rouhard@hotmail.fr>
2019-09-03 18:05:23 +0200

    feat(jordan): Add u1, u2, lambda1, lambda2

--- include/jordan.h
+++ include/jordan.h
@@ -6,6 +6,17 @@
 
 Matrix jordanSymMatrix(const std::vector<double>& values);
 
-double euclideanNorm(const std::vector<double>& x);
+double euclideanNorm(const std::vector<double>& x, unsigned startIndex=0);
+
+double lambda1(const std::vector<double>& x);
+
+double lambda2(const std::vector<double>& x);
+
+std::vector<double> u1(const std::vector<double>& x, int sgn);
+
+std::vector<double> getU1(const std::vector<double>& x);
+
+std::vector<double> getU2(const std::vector<double>& x);
+
 
 #endif
--- lib/jordan.cpp
+++ lib/jordan.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cmath>
 
 #include "jordan.h"
@@ -17,12 +18,46 @@ Matrix jordanSymMatrix(const std::vector<double>& x)
     return Matrix(nrows, ncols, values);
 }
 
-double euclideanNorm(const std::vector<double>& x)
+
+double euclideanNorm(const std::vector<double>& x, unsigned startIndex)
 {
     double sumOfSquares = 0;
-    for(const double& xi : x)
-    {
-        sumOfSquares += xi * xi;
-    }
+    for(auto xi{x.begin()+startIndex}; xi<x.end(); ++xi) sumOfSquares += *xi * *xi;
     return std::sqrt(sumOfSquares);
 }
+
+std::vector<double> _getU(const std::vector<double>& x, int sgn)
+{
+    double _euclideanNorm = euclideanNorm(x, 1);
+    auto n = x.size();
+    if (_euclideanNorm < 1e-12)
+    {
+        if (n > 1)
+        {
+            std::vector<double> _u(n, sgn * 0.5 * std::sqrt(1.0 / (n - 1)));
+            _u[0] = 0.5;
+            return _u;
+        }
+        else return std::vector<double>(1, 0.5);
+    }
+    else 
+    {
+        std::vector<double> _u = x;
+        double coefficient = sgn * 0.5 / _euclideanNorm;
+        std::transform(_u.begin(), _u.end(), _u.begin(), [coefficient](double& c){return c * coefficient;});
+        _u[0] = 0.5;
+        return _u;
+    }
+}
+
+
+std::vector<double> getU1(const std::vector<double>& x) {return _getU(x, -1);}
+
+
+std::vector<double> getU2(const std::vector<double>& x) {return _getU(x, 1);}
+
+
+double lambda1(const std::vector<double>& x) { return x[0] - euclideanNorm(x, 1); }
+
+
+double lambda2(const std::vector<double>& x) { return x[0] + euclideanNorm(x, 1); }
